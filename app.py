16040135bffaabfe1610111d22a0190eeca9972d
Brady Peterson <bradyrpeterson2@gmail.com>
2025-12-02 23:05:58 -0500

    Debugging firebase

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if not firebase_admin._apps:
         cred = credentials.Certificate('firebase_credentials.json')
     else:
         service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
-    
+        cred = credentials.Certificate(service_account_info)
     firebase_admin.initialize_app(cred)
 #Initialize my firestore DB
 db = firestore.client()
